bc512199c47e8b20fdf7b1289b96b5c04bbd1878
MMcClure11 <mcclme02@luther.edu>
2020-08-24 13:48:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+// // DOMContentLoaded: 
+
+// const main = document.querySelector("#main-content")
+// console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  // have the user input
+  const newTask = document.querySelector("#new-task-description").value
+  // slap it on the DOM 
+  // where? 
+  const taskItem = document.createElement("li")
+  taskItem.innerText = newTask
+
+  taskList.appendChild(taskItem)
+  taskForm.reset() //clears out form input
+})
