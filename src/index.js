30919a15027249d3e45edb70705becd69462d587
jordles113 <jordan.t.romero@gmail.com>
2020-08-25 12:51:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
 
-  const taskForm = document.querySelector("#create-task-form")
-  const taskList = document.querySelector("#tasks")
-  
-  taskForm.addEventListener("submit", function(e) {
-    e.preventDefault()
-    const newTask = document.querySelector("#new-task-description").value
-   
-    const taskItem = document.createElement("li")
-    taskItem.innerText = newTask
-  
-    taskList.appendChild(taskItem)
-    taskForm.reset() 
-  }); 
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  // have the user input
+  const newTask = document.querySelector("#new-task-description").value
+  const newTaskDueDate = document.querySelector("#new-task-due-date").value
+  const taskPriority = document.querySelector("#priority").value
+  // slap it on the DOM 
+  // where? 
+  taskList.innerHTML += `
+      <li> ${newTask.fontcolor(taskPriority)} - Due: ${newTaskDueDate}
+        <button data-action="delete"> X </button>
+      </li>
+   `
+    
+  taskForm.reset() 
+})
+
+taskList.addEventListener("click", function(e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove()
+  }
+})
+
