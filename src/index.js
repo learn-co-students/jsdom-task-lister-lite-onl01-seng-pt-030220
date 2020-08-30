ba472aefd9a21fd751da646a9d9aff47559baaa0
engahmed10 <eng.computer1@yahoo.com>
2020-08-30 15:36:53 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+const form = document.querySelector("#create-task-form")
 document.addEventListener("DOMContentLoaded", () => {
     // your code here
-    document.querySelector("#create-task-form").addEventListener("submit", function(e) {
-        var text = document.getElementById("new-task-description").value
-        var li = document.createElement("li")
-        var ul = document.getElementById("tasks")
-        li.innerHTML += text;
-        ul = ul.appendChild(li);
-        //if we don't do prevent default ,text will disapear
-        //e.preventDefault()
+    form.addEventListener("submit", function(e) {
+        createTask(e)
     })
 
-
-
 });
+
+function createTask(e) {
+    e.preventDefault()
+    const text = document.getElementById("new-task-description").value
+    const li = document.createElement("li")
+    const ul = document.getElementById("tasks")
+    const parent = document.getElementById("tasks")
+    li.innerHTML += text;
+    appendTask(li, parent)
+
+
+}
+
+function appendTask(newTask, parent) {
+    parent.appendChild(newTask)
+}
