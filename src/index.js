c8cffde1ec0f831f765b897d4b97a137b5f70988
Rkebede <rutht.kebede@gmail.com>
2020-08-30 17:32:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const form = document.getElementById("create-task-form")
+
+function print(){
+  const task = document.getElementById("new-task-description").value
+  let newTask = document.getElementById("tasks")
+  newTask.innerHTML = `<li>${task}</li>`
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  print(event.target["new-task-description"].value);
+  }, false);
 });
+
+
+console.log(task)
