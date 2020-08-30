d4c4e75a00e1dc26a3724d617e0a5ef1df2936ac
engahmed10 <eng.computer1@yahoo.com>
2020-08-29 22:54:56 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    document.querySelector("#create-task-form").addEventListener("submit", function(e) {
+        var text = document.getElementById("new-task-description").value
+        var li = document.createElement("li")
+        var ul = document.getElementById("tasks")
+        li.innerHTML += text;
+        ul = ul.appendChild(li);
+        //if we don't do prevent default ,text will disapear
+        //e.preventDefault()
+    })
+
+
+
 });
